beaeed40a6e62caf73075c8ebee5061e76ac3297
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2026-08-03 10:47:33 +0000

    token check

--- src/list.ts
+++ src/list.ts
@@ -1,3 +1,7 @@
+import { getTokenOrExit } from './token.js';
+
 export function printList(): void {
+  const token = getTokenOrExit();
   console.log('List of items...');
+  console.debug(`token: ${token}`);
 }
