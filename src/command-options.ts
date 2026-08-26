20ad74384c76bdeffc2c6a195f419013bc0ca6ce
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2026-08-26 08:38:50 +0000

    command options

--- src/command-options.ts
+++ src/command-options.ts
@@ -1,18 +1,26 @@
 interface BaseCommandOptions {
   assignedToMe: boolean;
-  requestedMyReview: boolean;
   authoredByMe: boolean;
   authoredByDependabot: boolean;
   authoredByRenovate: boolean;
-  notYetReviewed: boolean;
   limit: number;
   user: string;
   repo: string;
   label: string[];
 }
-export type ListCommandOptions = BaseCommandOptions
-
+export interface ListCommandOptions extends BaseCommandOptions {
+  requestedMyReview: boolean;
+  notYetReviewed: boolean;
+}
 export interface AutoReviewCommandOptions extends BaseCommandOptions {
+  requestedMyReview: boolean;
+  notYetReviewed: boolean;
+  allowNoChecks: boolean;
+  dryRun: boolean;
+  maxUpdate: number;
+  maxUpdatePerRepo: number;
+}
+export interface AutoMergeCommandOptions extends BaseCommandOptions {
   allowNoChecks: boolean;
   dryRun: boolean;
   maxUpdate: number;
@@ -20,16 +28,20 @@ export interface AutoReviewCommandOptions extends BaseCommandOptions {
 }
 
 export function buildPullRequestSearchQuery(
-  options: ListCommandOptions | AutoReviewCommandOptions,
+  options: ListCommandOptions | AutoReviewCommandOptions | AutoMergeCommandOptions,
+  withReviewApproved: boolean = false,
 ): string[] {
   const query: string[] = ['is:pr', 'is:open'];
+  if (withReviewApproved) {
+    query.push('review:approved');
+  }
   const filters: Array<[boolean, string]> = [
     [options.assignedToMe, 'assignee:@me'],
-    [options.requestedMyReview, 'user-review-requested:@me'],
+    ['requestedMyReview' in options && options.requestedMyReview, 'user-review-requested:@me'],
     [options.authoredByMe, 'author:@me'],
     [options.authoredByDependabot, 'author:app/dependabot'],
     [options.authoredByRenovate, 'author:app/renovate'],
-    [options.notYetReviewed, 'review:none'],
+    ['notYetReviewed' in options && options.notYetReviewed, 'review:none'],
     [Boolean(options.user), `user:${options.user}`],
     [Boolean(options.repo), `repo:${options.repo}`],
   ];
